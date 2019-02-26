8c2d8ba22b97d6b0a307f0abbdb47dd1177a48fb
cusinh <cusinh2606>
2019-02-26 10:41:05 +0700

    sinh commit

--- src/main/java/Barcode/CreateBarcode.java
+++ src/main/java/Barcode/CreateBarcode.java
@@ -1,5 +1,77 @@
 package Barcode;
 
-public class CreateBarcode {
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Reader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+public class CreateBarcode {
+	String pathFile = "C:\\Users\\Sinh\\Desktop\\";
+
+	public static void createImageBarcode(String image_name, String myString, String pathFile) {
+
+		try {
+			Code128Bean code128 = new Code128Bean();
+			code128.setHeight(15f);
+			code128.setModuleWidth(0.3);
+			code128.setQuietZone(10);
+			code128.doQuietZone(true);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			BitmapCanvasProvider canvas = new BitmapCanvasProvider(baos, "image/x-png", 300,
+					BufferedImage.TYPE_BYTE_BINARY, false, 0);
+			code128.generateBarcode(canvas, myString);
+			canvas.finish();
+
+			// write to png file
+			FileOutputStream fos = new FileOutputStream(pathFile + image_name);
+			fos.write(baos.toByteArray());
+			fos.flush();
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String createImageToCode(String imageName, String pathFile) {
+		String result1 = null;
+		try {
+			InputStream barCodeInputStream = new FileInputStream(pathFile + imageName);
+			BufferedImage barCodeBufferedImage = ImageIO.read(barCodeInputStream);
+
+			LuminanceSource source = new BufferedImageLuminanceSource(barCodeBufferedImage);
+			BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+			Reader reader = new MultiFormatReader();
+			Result result = reader.decode(bitmap);
+			result1 = result.getText();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result1;
+	}
+
+	public static void main(String[] args) throws NotFoundException, ChecksumException, FormatException, IOException {
+		// createImageBarcode("test.png", "123123123");
+		// System.out.println("finished");
+
+		// imageToCode("test.png");
+	}
 }
--- src/main/java/QRCode/CreateQRCode.java
+++ src/main/java/QRCode/CreateQRCode.java
@@ -1,5 +1,68 @@
 package QRCode;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 public class CreateQRCode {
+
+	@SuppressWarnings("unchecked")
+	public static String InputImage(String filePath) throws FileNotFoundException, NotFoundException, IOException {
+		String charset = "utf-8"; // or "ISO-8859-1"
+		@SuppressWarnings("rawtypes")
+		Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		return readQRCode(filePath, charset, hintMap);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static String readQRCode(String filePath, String charset, Map hintMap) throws FileNotFoundException, IOException, NotFoundException {
+
+			BinaryBitmap binaryBitmap = new BinaryBitmap(
+					new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filePath)))));
+			Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap);
+			return qrCodeResult.getText();
+
+	}
+	
+	public void TextToQR(String data, String pathFile, String nameImage) throws WriterException, IOException {
+		// String data = "Super Pig...";
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix matrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 200, 200);
+
+		// Write to file image
+		String outputFile = pathFile + nameImage + ".jpg";
+		Path path = FileSystems.getDefault().getPath(outputFile);
+		MatrixToImageWriter.writeToPath(matrix, "PNG", path);
+		System.out.println("QR Code created!");
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public String QRToText(String filePath) throws FileNotFoundException, NotFoundException, IOException {
+		String charset = "utf-8";
+		Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		return readQRCode(filePath, charset, hintMap);
+	}
 
 }
